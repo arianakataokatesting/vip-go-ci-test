a7b7ac30c4fa145c8a682e37d099d48236e0d5a5
Ariana Kataoka <aris.kataoka@gmail.com>
2021-04-29 13:41:17 +1000

    Itroduce errors

---
+++ src/MyTestClass.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class MyTestClass
+{
+    protected string $test;
+
+    public function test(): string
+    {
+        foreach ($_POST['menu-locations'] as $menu_id) {
+            echo 'ADD warning severity 10 - non-sanitized input variable';
+        }
+        $functionsCookies = $_COOKIE;
+
+        $functionsPost = $_POST;
+        var_dump($functionsPost);
+
+        session_write_close();
+    }
+}
